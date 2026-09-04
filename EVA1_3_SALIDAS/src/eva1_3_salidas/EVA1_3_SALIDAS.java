38c41cbd48581bfd491dadf4c2ae963ecb6b42f5
Ivonne Victoria Nevarez Moreno <L26550280@chihuahua2.tecnm.mx>
2026-09-03 18:43:20 -0600

    Add files via upload

---
+++ EVA1_3_SALIDAS/src/eva1_3_salidas/EVA1_3_SALIDAS.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_3_salidas;
+
+/**
+ *
+ * @author bisonte
+ */
+public class EVA1_3_SALIDAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bienvenido a Fundamentos de Programación");
+        System.out.println("Ivonne Victoria Nevarez Moreno");
+        System.out.print("Tu calificacion es:");
+        System.out.println(85);
+        
+        // TODO code application logic here
+    }
+    
+}
