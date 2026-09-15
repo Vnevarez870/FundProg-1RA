28f9ad68be5bb72e2d75b04f98d4850d398b484e
Ivonne Victoria Nevarez Moreno <L26550280@chihuahua2.tecnm.mx>
2026-09-14 18:58:31 -0600

    Add files via upload

---
+++ EVA1_7_OPERACIONES/src/eva1_7_operaciones/EVA1_7_OPERACIONES.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_7_operaciones;
+
+/**
+ *
+ * @author bisonte
+ */
+public class EVA1_7_OPERACIONES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //EL TIPO DE DATO DETERMINA LAS OPERACIONES
+        //ENTEROS, REALES, CADENAS, LOGICOS, ETC.
+        //EXPRECIÓN:
+        //VALORES
+        //VARIABLES
+        //OPERACIONES 
+        //FUNCIONES 
+        //QUE SE EVALUAN PARA OPTENER UN RESULTADO 
+        //OPERACIONES CON NÚMEROS:
+        //ARITMÉTICAS:
+        int suma, val1,val2; //DECLARO 3 VARIABLES DEL MISMO TIPO 
+        //ASIGNACION: PONER UN VALOR EN UNA VARIABLE 
+        //OPERADOR --> SIMBOLO QUE TRABAJA SOBRE IDENTIFICADORES.
+        // = -->  ASIGNACIÓN --> PONER VALOR
+        // == --> ASIGNACIÓN
+        val1 = 100; //ASIGNAR EL VALOR DE 100 A LA VARIABLE VA11
+        val2 = 50; 
+        suma = val1 + val2; //EXPRESIÓN, ASIGNAR LA VARIABLE SUMA LA OPERACIÓN ENTRE VAL1 Y VAL2
+        //+ --> ESTA ENTRE DOS ENTERPS SE INTERPRETA COMO QUE LOS DEBE SUMAR.
+        
+        
+        System.out.print("val1 = ");
+        System.out.println(val1);
+        System.out.print("val2 = ");
+        System.out.println("val2");
+        System.out.print("suma = ");
+        System.out.println("suma");
+        //OPERACIONES RESPETAN REGLAS MATEMATICAS 
+        //MULTIPLICACIÓN 
+        //*
+        int multi;
+        multi = val1 * val2;
+        System.out.print("Multiplicación =");
+        System.out.println(multi);
+        //DIVICIÓN
+        // --> OPERACION CON DIVICIÓN 
+        int divi;
+        divi = val1 / val2;
+        System.out.print( "Divición =");
+        System.out.println("divi");
+        val1 = 10;
+        val2 = 3;
+        divi = val1 / val2;
+        System.out.print("val1 = ");
+        System.out.println(val1);
+        System.out.print(" val2 =  ");
+        System.out.println(val2);
+        System.out.println(divi);
+        double resu;
+        resu = val1 / val2 ;
+        System.out.print("Resultado =");
+        System.out.println(resu);
+        double val2copia;
+        val2copia=3;
+        resu = val1 /val2copia;
+        System.out.print("Resultado =");
+        System.out.println(resu);
+           
+           
+           
+        
+    }
+    
+}
---
+++ EVA1_9_OPERACIONES_CADENAS/src/eva1_9_operaciones_cadenas/EVA1_9_OPERACIONES_CADENAS.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_9_operaciones_cadenas;
+
+/**
+ *
+ * @author bisonte
+ */
+public class EVA1_9_OPERACIONES_CADENAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //TAMBIEN HAY OPERADORES PARA CADENAS
+        //CONCATENACIÓN: PEGAR CADENAS DE TEXTO --> +
+        String cade ;
+        cade = "Hola" + " " + "mundo";
+        System.out.println(cade);
+        cade = "Valor  =  " + 100;
+        System.out.println(cade);
+        cade = "Valores booleanos = " + false;
+        System.out.println (cade);
+    }
+    
+}
